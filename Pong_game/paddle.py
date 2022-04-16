1f156e2d6fd4b1cb6075af30d00ca7fd1dec4103
SAI <103210783+iSAI552@users.noreply.github.com>
2022-04-16 22:42:02 +0530

    Add files via upload
    It is a multiplayer ping pong game controlled with 'w', 's', 'up' and 'down' keys.

---
+++ Pong_game/paddle.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self, x_cor, y_cor):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(x=x_cor, y=y_cor)
+    def go_up(self):
+        if self.ycor() < 250:
+            new_y = self.ycor() + 20
+            self.goto(x=self.xcor(), y=new_y)
+
+    def go_down(self):
+        if -250 < self.ycor():
+            new_y = self.ycor() - 20
+            self.goto(x=self.xcor(), y=new_y)
+
+
+
+
+
